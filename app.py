dda8ba3b01bd91cfa71edc13f3d8aecf5ac1943c
HungNguyen686868 <nguyenphunghung1511@gmail.com>
2022-10-25 03:25:34 +0000

    make lint wrong config

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
  
 @app.route("/hello", methods=["GET"])
 def hello():
-    return 'Hello Udacity. Devops here'
+    return 'Hello Udacity. Devops here'xxxx
  
 app.run(host="0.0.0.0", port=80, debug=True)
